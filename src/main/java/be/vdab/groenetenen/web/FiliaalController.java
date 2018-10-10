e0a2e25db865bdeabaada2afb1443398fff9821f
Jimmy <Jimmy@PC-JIMMY.home>
2018-10-10 10:35:35 +0200

    3.1.1 Automatische String naar Entity converters

--- src/main/java/be/vdab/groenetenen/web/FiliaalController.java
+++ src/main/java/be/vdab/groenetenen/web/FiliaalController.java
@@ -1,38 +1,57 @@
 package be.vdab.groenetenen.web;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import be.vdab.groenetenen.entities.Filiaal;
 import be.vdab.groenetenen.services.FiliaalService;
 
 @Controller
-@RequestMapping(value = "filialen")
+@RequestMapping(value = "/filialen")
 class FiliaalController {
 	
 	private static final String VAN_TOT_POSCODE_VIEW = "filialen/vantotpostcode";
+	private static final String FILIAAL_VIEW = "filialen/filiaal";
+	private static final String REDIRECT_FILIAAL_NIET_GEVONDEN = "redirect:/";
 	private final FiliaalService filiaalService;
 	
 	FiliaalController(FiliaalService filiaalService){
 		this.filiaalService = filiaalService;
 	}
 	
-	@GetMapping("vantotpostcode")
+	@GetMapping(value = "/vantotpostcode")
 	ModelAndView vanTotPostcode() {
-	return new ModelAndView(VAN_TOT_POSCODE_VIEW)
-			.addObject("vanTotPostcodeForm", new VanTotPostcodeForm());
+		return new ModelAndView(VAN_TOT_POSCODE_VIEW)
+				.addObject("vanTotPostcodeForm", new VanTotPostcodeForm());
 	}
+	
+	
 	@GetMapping(params = { "van", "tot" })
 	ModelAndView vanTotPostcode(@Valid VanTotPostcodeForm form, BindingResult bindingResult) {
-	if (bindingResult.hasErrors()) {
-		return new ModelAndView(VAN_TOT_POSCODE_VIEW);
+		if (bindingResult.hasErrors()) {
+			return new ModelAndView(VAN_TOT_POSCODE_VIEW);
+		}
+		return new ModelAndView(VAN_TOT_POSCODE_VIEW)
+				.addObject("filialen", filiaalService.findByPostcode(
+						form.getVan(), form.getTot()));
 	}
-	return new ModelAndView(VAN_TOT_POSCODE_VIEW)
-			.addObject("filialen", filiaalService.findByPostcode(
-					form.getVan(), form.getTot()));
+	
+	
+	@GetMapping(value = "{filiaal}") 
+	ModelAndView read(@PathVariable Optional<Filiaal> filiaal, RedirectAttributes redirectAttributes) {
+		if (filiaal.isPresent()) {
+			return new ModelAndView(FILIAAL_VIEW).addObject(filiaal.get());
+		}
+		redirectAttributes.addAttribute("fout", "Filiaal niet gevonden");
+		return new ModelAndView(REDIRECT_FILIAAL_NIET_GEVONDEN);
 	}
 }
